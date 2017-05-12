d07a0593318de25fc4966538a72b7ab495002368
Marcelo Sampaio <marcelo@artlogica.com.br>
2017-05-12 16:53:58 -0300

    initial commit

--- Permissions/ViewController.m
+++ Permissions/ViewController.m
@@ -8,22 +8,59 @@
 
 #import "ViewController.h"
 
+#import <ClusterPrePermissions/ClusterPrePermissions.h>
+
+
+
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
 
+
+
+#pragma mark - View Life Cycle
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // Prepare you info.plist
+//    NSCalendarsUsageDescription
+//    NSPhotoLibraryUsageDescription
+//    NSCameraUsageDescription
+//    NSMicrophoneUsageDescription
+//    NSCalendarsUsageDescription
+//    NSLocationWhenInUseUsageDescription
+//    NSLocationAlwaysUsageDescription
+    
+    
+    
+    // Permissions Framework
+    ClusterPrePermissions *permissions = [ClusterPrePermissions sharedPermissions];
+    [permissions showPhotoPermissionsWithTitle:@"Access your photos?"
+                                       message:@"Your message here"
+                               denyButtonTitle:@"Not Now"
+                              grantButtonTitle:@"Give Access"
+                             completionHandler:^(BOOL hasPermission,
+                                                 ClusterDialogResult userDialogResult,
+                                                 ClusterDialogResult systemDialogResult) {
+                                 if (hasPermission) {
+                                     // Continue with your code here
+                                     NSLog(@"Permission granted! ");
+                                 } else {
+                                     // Handle access not being available
+                                     NSLog(@"You don't have permissions");
+                                 }
+                             }];
+    
+    
+
+    
+    
+    
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 
 @end
