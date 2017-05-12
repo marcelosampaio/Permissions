de4a9b8ff86632252ab589907a3f7266fb89958e
Marcelo Sampaio <marcelo@artlogica.com.br>
2017-05-12 15:32:40 -0300

    Initial Commit

---
+++ Permissions/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Permissions
+//
+//  Created by Marcelo Sampaio on 5/12/17.
+//  Copyright © 2017 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
